460bde80d10a0749e9151e36590ca9ce2c08df8c
devarctic11-commits <devarctic11@gmail.com>
2025-08-09 00:03:50 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Courses from './pages/Courses';
+import Chatbot from './pages/Chatbot';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
